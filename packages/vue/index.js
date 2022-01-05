06970c55a929a60742f620c4abe401402548a2a7
Techassi <git@techassi.dev>
2022-01-05 15:57:01 +0100

    Add Vue config

---
+++ packages/vue/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable unicorn/prefer-module */
+module.exports = {
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+      rules: {
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+  ],
+  extends: ['plugin:vue/vue3-recommended', '@techassi/eslint-config-ts'],
+  rules: {
+    'vue/no-v-html': 'warn',
+  },
+};
